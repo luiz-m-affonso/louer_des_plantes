0449b421da45ae1878dea92e633b5b20fbe672eb
Insight-Creator <luizbuchhalter@Outlook.com>
2020-10-24 16:41:27 -0300

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,30 @@ ActiveRecord::Schema.define(version: 2020_10_24_192131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "plant_id", null: false
+    t.string "start_date"
+    t.string "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plant_id"], name: "index_bookings_on_plant_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "plants", force: :cascade do |t|
+    t.string "common_name"
+    t.string "scientific_name"
+    t.string "family"
+    t.string "image_url"
+    t.integer "price"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plants_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +51,7 @@ ActiveRecord::Schema.define(version: 2020_10_24_192131) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "plants"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "plants", "users"
 end
