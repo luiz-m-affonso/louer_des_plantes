77d5afcd992faf8253b7dddd9ced84fe17bfbbba
Insight-Creator <luizbuchhalter@Outlook.com>
2020-10-26 00:01:39 -0300

    Clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   resources :plants do
     resources :bookings, only: [:new, :create, :destroy]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
