b23a1b2c440927b9250206fd431d64697c325f31
Insight-Creator <luizbuchhalter@Outlook.com>
2020-10-27 16:54:48 -0300

    Add CRUD for plants controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,43 @@
 class PlantsController < ApplicationController
+  before_action :set_plant, only: [:show, :edit, :update, :destroy]
+  def index
+    @plants = Plant.all
+  end
+
+  def show; end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.create(plant_params)
+    if @plant.save
+      redirect_to plant_path(@plant)
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    @plant.update(plant_params)
+    redirect_to plant_path(@plant)
+  end
+
+  def destroy
+    @plant.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:common_name, :scientific_name, :family, :image_url, :price, :address)
+  end
 end
