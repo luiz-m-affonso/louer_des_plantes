9baaeec2ef0958a78672135e5b6b4b2afd71d433
daniel assis <daniel.augusto.assis@gmail.com>
2020-10-27 20:45:01 -0300

    seed api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+create_table "plants", force: :cascade do |t|
+    t.string "common_name"
+    t.string "scientific_name"
+    t.string "family"
+    t.string "image_url"
+    t.integer "price"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plants_on_user_id"
+  end
+
+
+require 'open-uri'
+require 'json'
+
+token = '2U0o7bEHdfONF17LTVcbSRhIUc_KyCdBLBmyc2MbPQw'
+
+response =
+
+puts "start seeding"
+
+
+
+
+puts "finish seeding"
