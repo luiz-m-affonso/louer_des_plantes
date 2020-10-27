df0ef515c633b3c5dcd8e8b9eb225be62cee836a
Insight-Creator <luizbuchhalter@Outlook.com>
2020-10-27 17:12:17 -0300

    Add create for booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,4 +4,14 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new(booking_params)
   end
+
+  def create
+    @plant = Plant.find(params[:plant_id])
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      redirect_to plant_path(@plant)
+    end
+  end
 end
