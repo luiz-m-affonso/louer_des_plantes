8b22ba4b8e126f21cbb1f6d4d17d825ad2a6d099
Julia Mathias <juliamss@gmail.com>
2020-10-30 09:50:43 -0300

    Added index to home but made a mess

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    @plants = policy_scope(Plant).order(created_at: :desc)
   end
 
   def components
