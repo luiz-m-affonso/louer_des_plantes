2ee4b6901bc2e1263480e65890974803b325bbc6
Julia Mathias <juliamss@gmail.com>
2020-10-31 12:50:53 -0300

    Podemos ver o show sem estar logado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:home, :comṕonents]
 
   def home
     @plants = policy_scope(Plant).order(created_at: :desc)
