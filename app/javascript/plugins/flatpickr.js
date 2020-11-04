fd8cba4b7f7d61fd3af64a0320a5a0b969985e73
Julia Mathias <juliamss@gmail.com>
2020-11-04 09:57:10 -0300

    Added pretty calendar picker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,3 +1,9 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-flatpickr(".datepicker", {});
+
+flatpickr("#range_start", {
+  altInput: true,
+  enableTime: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+});
