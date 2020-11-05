5b82f055db8211c91cf7be9d19dd4832d859525a
Insight-Creator <luizbuchhalter@Outlook.com>
2020-11-05 18:32:49 -0300

    Add Map to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :comṕonents]
+  skip_before_action :authenticate_user!, only: [:home, :components, :map]
 
   def home
     @plants = policy_scope(Plant).order(created_at: :desc)
@@ -11,5 +11,16 @@ class PagesController < ApplicationController
   def profile
     @bookings = Booking.where(user_id: current_user.id)
     @plants = Plant.where(user_id: current_user.id)
+  end
+
+  def map
+    @plants = Plant.all
+    @markers = @plants.geocoded.map do |plant|
+      {
+        lat: plant.latitude,
+        lng: plant.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { plant: plant })
+      }
+    end
   end
 end
