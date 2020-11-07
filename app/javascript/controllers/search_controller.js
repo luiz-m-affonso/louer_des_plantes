291df963dce15bb453d3524e96b8c1d104f65baf
Julia Mathias <juliamss@gmail.com>
2020-11-06 23:42:01 -0300

    Made a bunch of cosmetic fixes

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ 'main' ];
+
+  append() {
+    console.log('hi dummie');
+    console.log(this.mainTarget)
+  //   this.mainTarget.insertAdjacentHTML('afterend', <%= render "plants/index", plants: @plants %>);
+  // }
+}
